79ed9abe8c849585448243aec1256de0d071288c
jaffrantirta <franartika@gmail.com>
2023-03-15 22:48:08 +0800

    is visibale

--- src/components/Coundown.js
+++ src/components/Coundown.js
@@ -1,9 +1,9 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef } from 'react'
 import CardCountdown from './CardCoundown';
 
 export default function Countdown({ venue }) {
     const sectionRef = useRef(null);
-    const [isVisible, setIsVisible] = useState(false);
+    // const [isVisible, setIsVisible] = useState(false);
     const targetDate = {
         apuan: {
             date: '2023-04-16T00:00:00Z'
