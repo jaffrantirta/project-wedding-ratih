795e1fdd9378025367aef54df9b9883afcc4c265
jaffrantirta <franartika@gmail.com>
2023-03-17 11:43:07 +0800

    update name groom and bride

--- src/sections/GroomBride.js
+++ src/sections/GroomBride.js
@@ -34,7 +34,7 @@ export default function GroomBride({ venue }) {
                 <div className={`text-center p-10`}>
                     <img alt='' src={ImgDevider} className={`w-1/2 md:w-1/4 mx-auto transition-all duration-1000 ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-32'}`} />
                     <img alt='' src={ImgOswastyastu} className={`w-1/2 md:w-1/6 mx-auto transition-all duration-700 ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-32'}`} />
-                    <p className={`md:px-10 my-5 transition-all duration-1000 ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-32'}`}>Atas Asung Kertha Wara Nugraha Ida Sang Hyang Widhi Wasa/Tuhan Yang Maha Esa kami bermaksud mengundang Bapak/Ibu/Saudara/i pada Upacara Manusa Yadnya Pawiwahan (Pernikahan) {venue == 'denpasar' ? '' : 'dan Mepandes (Potong Gigi)'} </p>
+                    <p className={`md:px-10 my-5 transition-all duration-1000 ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-32'}`}>Atas Asung Kertha Wara Nugraha Ida Sang Hyang Widhi Wasa/Tuhan Yang Maha Esa kami bermaksud mengundang Bapak/Ibu/Saudara/i pada Upacara Manusa Yadnya Pawiwahan (Pernikahan) {venue === 'denpasar' ? '' : 'dan Mepandes (Potong Gigi)'} </p>
                     <div className='grid grid-cols-1 md:grid-cols-2 mt-14'>
                         <div className={`flex justify-center`}>
                             <img
@@ -43,7 +43,7 @@ export default function GroomBride({ venue }) {
                                 className={`w-full md:w-1/2 h-auto transition-all duration-300 ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-32'}`} />
                         </div>
                         <div className={`transition-all duration-200 ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-32'}`}>
-                            <h1 className='text-2xl md:text-5xl font-third mb-10 text-primary'>I Gede Ananda Eka Diana</h1>
+                            <h1 className='text-2xl md:text-5xl font-third mb-10 text-orange-500'>I Gede Ananda Eka Diana</h1>
                             <h1 className='text-xl mt-8'>Putra pertama dari empat bersaudara pasangan</h1>
                             <h1 className='text-md'>I Nyoman Kusuma Yudiana</h1>
                             <h1 className='text-md'>&</h1>
@@ -62,7 +62,7 @@ export default function GroomBride({ venue }) {
                                 className={`w-full md:w-1/2 h-auto transition-all duration-300 ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-32'}`} />
                         </div>
                         <div className={`transition-all duration-500 ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-32'}`}>
-                            <h1 className='text-2xl md:text-5xl font-third mb-10 text-primary'>Luh Gede Ratih Dian Rahayu</h1>
+                            <h1 className='text-2xl md:text-5xl font-third mb-10 text-orange-500'>Luh Gede Ratih Dian Rahayu</h1>
                             <h1 className='text-xl mt-8'>Putri pertama dari tiga bersaudara pasangan</h1>
                             <h1 className='text-md'>I Made Sunantra Yasa</h1>
                             <h1 className='text-md'>&</h1>
